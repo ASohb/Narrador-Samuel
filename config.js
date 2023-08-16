c9ebda97b7ec69e3e69053477e86f5c2dd499aaa
ASohb <92811009+ASohb@users.noreply.github.com>
2023-08-16 17:01:14 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAVNTzjSOuIUYN8H6SLi6s92z-t0A2xpH0",
+    authDomain: "narradorsamuel-532eb.firebaseapp.com",
+    projectId: "narradorsamuel-532eb",
+    storageBucket: "narradorsamuel-532eb.appspot.com",
+    messagingSenderId: "477190235783",
+    appId: "1:477190235783:web:052c133e2507fe6a287a22"
+  };
---
+++ navigation/DrawerNavigator.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import TabNavigator from "./TabNavigator";
+import Profile from "../screens/Profile";
+import StackNavigator from './StackNavigator';
+
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+     return(
+        <Drawer.Navigator>
+            <Drawer.Screen name="Tela Inicial" component={StackNavigator} />
+            <Drawer.Screen name="Perfil" component={Profile} />
+
+        </Drawer.Navigator>
+     );
+}
+
+export default DrawerNavigator;
